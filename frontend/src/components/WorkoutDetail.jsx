d44b8111474e472f716dbe93da610527fa047b5e
Mohamed ED-DAFALI <66277987+edafali@users.noreply.github.com>
2024-09-11 20:40:53 +0100

    Update WorkoutDetail.jsx

--- frontend/src/components/WorkoutDetail.jsx
+++ frontend/src/components/WorkoutDetail.jsx
@@ -9,7 +9,7 @@ export default function WorkoutDetail({ workout }) {
   const trash = <FontAwesomeIcon icon={faTrash} />;
   const handlDelete = async () => {
     const response = await fetch(
-      "http://localhost:4000/api/workouts/" + workout._id,
+      "https://wrokouts-app-backend.onrender.com/api/workouts/" + workout._id,
       {
         method: "DELETE",
       }
